de4fbd9c909897e80aff98ddb53f385e02bf75ec
Nadav <nadavvivsa@gmail.com.com>
2023-01-13 21:11:22 +0200

    try number 3

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const path = require("path");
 
 const app = express();
 const http = require("http").createServer(app);
@@ -9,30 +7,10 @@ const http = require("http").createServer(app);
 app.use(cookieParser());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  // Express serve static files on production environment
-  app.use(express.static(path.resolve(__dirname, "public")));
-} else {
-  // Configuring CORS
-  const corsOptions = {
-    // Make sure origin contains the url your frontend is running on
-    origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
-}
-
 // routes
 const restaurantRoutes = require("./api/restaurant/restaurant.routes");
 
 app.use("/api/restaurant", restaurantRoutes);
-
-// Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/wap/123 it will still respond with
-// our SPA (single page app) (the index.html file) and allow vue-router to take it from there
-app.get("/**", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 const logger = require("./services/logger.service.js");
 const port = process.env.PORT || 3030;

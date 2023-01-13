5b7f8b2693ace658e03358dc104241ff33c1b37b
Nadav <nadavvivsa@gmail.com.com>
2023-01-13 21:22:19 +0200

    try number 4

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
 
 const app = express();
 const http = require("http").createServer(app);
 // Express App Config
-app.use(cookieParser());
 app.use(express.json());
+app.use(cors({ origin: "*", credentials: true }));
 
 // routes
 const restaurantRoutes = require("./api/restaurant/restaurant.routes");
